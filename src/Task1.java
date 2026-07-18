c5fae0273a89d3dd239713402ed70d52fddfc8a4
PageHun <sarsenbekovsanzar@gmail.com>
2026-07-19 02:33:15 +0500

    Solution of task1

---
+++ src/Task1.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<ArrayList<Integer>> lines = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int k = scanner.nextInt();
+            ArrayList<Integer> line = new ArrayList<>();
+            for (int j = 0; j < k; j++) {
+                line.add(scanner.nextInt());
+            }
+            lines.add(line);
+        }
+
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            try {
+                System.out.println(lines.get(x - 1).get(y - 1));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("ERROR!");
+            }
+        }
+    }
+}
